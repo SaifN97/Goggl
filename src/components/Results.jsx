750d21b9491b92eec8fc3f7e6928df118dada589
SaifN97 <saifnarpali@gmail.com>
2022-01-24 14:52:05 +0530

    search bar and navigation links

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -69,12 +69,12 @@ const Results = () => {
                 <p className="text-lg dark:text-blue-300 text-blue-700">
                   {title}
                 </p>
-                <div className="flex gap-4">
-                  <a href={source?.href} target="_blank" rel="noreferrer">
-                    {source?.href}
-                  </a>
-                </div>
               </a>
+              <div className="flex gap-4">
+                <a href={source?.href} target="_blank" rel="noreferrer">
+                  {source?.href}
+                </a>
+              </div>
             </div>
           ))}
         </div>
@@ -84,14 +84,14 @@ const Results = () => {
         <div className="flex flex-wrap">
           {results.map((vid, idx) => (
             <div key={idx} className="p-2">
-              {
+              {vid.additional_links?.[0]?.href && (
                 <ReactPlayer
                   url={vid.additional_links?.[0].href}
                   controls
                   width="355px"
                   height="200px"
                 />
-              }
+              )}
             </div>
           ))}
         </div>
